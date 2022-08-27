3c919298bad864c1076f118ab8d880a9c2410e9a
rvanderlely <101234963+rvanderlely@users.noreply.github.com>
2022-08-27 16:33:25 -0500

    Final push before handing in project

--- gcd_by_prime_factoring.cpp
+++ gcd_by_prime_factoring.cpp
@@ -2,116 +2,108 @@
 Name: Rachel Vanderlerly
 Program 1 GCD By Prime Factorization
 Date: August 21,2022
-Purpose: This program will have two functions which calculate the gcd using 
-the prime factorization method
+Purpose: This program uses two functions to calculate the gcd of two integers input by the user 
+and returns the greatest common divisor between the two. 
 */
 
 #include <iostream>
-
+/*------------------------------------------------------------
+                        Get_Prime_Factors
+Input:  An integer, x. 
+        An empty integer array passed by reference, factors.
+        An integer passed by reference, size.
+Output: None. Changes made to size and factors are done by reference. 
+------------------------------------------------------------*/
 void Get_Prime_Factors(int x, int factors[], int &size)
 {   
-    size = 0;
-    int prime_factor = 2;
+    size = 0;                               //Initialize size to 0
+    int prime_factor = 2;                   //Start with 2 as the prime factor
     while (x != 1)
     {
-        if (x % prime_factor == 0)
+        if (x % prime_factor == 0)          //if it is divisible by the prime factor
         {
-            factors[size++] = prime_factor;
-            x = x / prime_factor;
+            factors[size++] = prime_factor; //Add prime_factor to the array of prime factors and incriment size
+            x = x / prime_factor;           //Set x to x divided by the prime factor 
         }
         else
         {
-            prime_factor++;
+            prime_factor++;                 //If not divisible by prime factor then incriment prime factor
         }
     }
     
 }
-
 /*------------------------------------------------------------
                         Get Matches
-Input: 2 two sorted arrays and their lengths.
-Output: The GCD of two non negative numbers.
+Input:  An integer array passed by reference and it's length, L1 and length1.
+        An integer array passed by reference and it's length, L2 and length2.
+Output: Returns the GCD of two non negative numbers.
 ------------------------------------------------------------*/
-void get_gcd(int L1[],int length1, int L2[], int length2)
+int get_gcd(int L1[],int length1, int L2[], int length2)
 {
     int comparisions = 0;   //The number of comps made
     int matches =0;         //The number of matches found
     int index1 = 0 ;        //The index used to iterate through list 1
     int index2 = 0;         //The index used to iterate through list 2
-    int gcd = 1;
+    int gcd = 1;            //Initilize gcd with 1 
+    int L3[30];             //An array to hold the common prime factors
     int index2_found = 0;   //The index used to keep track of the last element match found 
                             //If a match is found then since both lists are sorted then 
                             //no elements before that index will need to be checked so you 
                             //can cut # of comps down signifigantly by using this as the new index
-    int L3[length1]; //Make this better later by doing lowest length of the two
-    //static int L3[length1]; //Make this better later by doing lowest length of the two
 
-
-    //Walks through List1 
+    //Step through List1 
     for (index1 = 0; index1 < length1;index1++)
     {
-
-        //Walks through List2 
+        //Step through List2 only checking from the index of the last found match, index2_found
         for (index2 = index2_found; index2 < length2; index2++)
         {
             comparisions++;                 //Incriment for each comparision
-            
-            if (L2[index2] == L1[index1])   //If a match is found
+            if (L2[index2] == L1[index1])   //If a match is found they share a prime factor
                 {
-                index2_found = index2 + 1;  //if there is a match then dont check anything before that index
-                L3[matches] = L2[index2];   //Save the matching number in List3
+                index2_found = index2 + 1;  //If there is a match then dont check anything before that index from now on
+                L3[matches] = L2[index2];   //Save the matching number in L3
                 matches++;                  //Incirment matches
                 break;                      //Break out of the second for loop because item has been found. 
                 }
         }
     }
-
-    //Print the results 
-    std::cout<<"Array1 :";
-    for (int i = 0;i<length1; i++){std::cout<<L1[i]<<"\t";}
-    std::cout<<"\nArray2 :";
-    for (int i = 0;i<length2; i++){std::cout<<L2[i]<<"\t";}
-    std::cout << "\nMatching elements:";
-    for (int l = 0;l<matches; l++){std::cout<<L3[l]<<"\t";}
-    std::cout<<"\nTotal Comparisions:"<< comparisions<<"\n";
-
     for (int i = 0; i < matches ; i++)
     {
-        gcd = gcd * L3[i];
+        gcd = gcd * L3[i];                  //Calculate the gcd by multiplying common prime factors
     }
-    std::cout<<"The gcd is :"<<gcd<< ".\n";
+    return gcd;                             //Return the gcd. 
 }
 
 int main()
 {
-
+char run_again = 'Y';
+while (run_again == 'Y' ||run_again == 'y')
+{
     //Get prime factors of first number
     int MAX_PRIMES = 30;
     int size1 = 0;
+    int number1 = 0;
+    std::cout<<"Enter number 1:";
+    std::cin>>number1;
     int factors1[MAX_PRIMES];
-    Get_Prime_Factors(24,factors1,size1);
-    std::cout<<"\nFirst number has "<<size1<<" prime factors";
-    std::cout<<"The prime factors are :";
-    for (int i = 0; i< size1;i++)
-    {
-        std::cout<<factors1[i]<<" ";
-    } 
+    Get_Prime_Factors(number1,factors1,size1);
 
     //Get prime factors of second number
     int size2 = 0;
     int factors2[MAX_PRIMES];
-    Get_Prime_Factors(119,factors2,size2);
-    std::cout<<"\nSecond number has "<< size2 <<" prime factors";
-    std::cout<<"The prime factors are :";
-    for (int i = 0; i< size2;i++)
-    {
-        std::cout<<factors2[i]<<" ";
-    } 
-    std::cout<<"\n"; 
+    int number2 =0;
+    std::cout<<"Enter number 2:";
+    std::cin>>number2;
+    Get_Prime_Factors(number2,factors2,size2);
 
+    //Find Gcd
+    int gcd = get_gcd(factors1,size1,factors2,size2);
+    std::cout<<"The GCD of "<<number1 <<" and " << number2 << " is:"<<gcd<< ".\n";
+    std::cout<<"Enter Y to run again or enter any other key to quit:";
+    std::cin>>run_again;
+    std::cout<<"\n";
+}
 
-    //Find the common elements
-    get_gcd(factors1,size1,factors2,size2);
     return 0;
 }
 
